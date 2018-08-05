7acfd82a567b9fd8e88029e7cd103c94ceabed8b
Xin Wang <angelapan.lu@gmail.com>
2018-08-05 10:25:48 -0500

    Create BalancedBST.java

---
+++ tree/BalancedBST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class BalancedBST {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isBalanced(root.left) || !isBalanced(root.right)) {
+            return false;
+        }
+        if (Math.abs(depth(root.left) - depth(root.right)) >= 2) {
+            return false;
+        } 
+        return true;
+    }
+    
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.max(depth(root.left), depth(root.right))+1;
+    }
+}
