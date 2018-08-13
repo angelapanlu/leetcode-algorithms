8491008506500ebdaa1b57be5adefdd3648572ef
Xin Wang <angelapan.lu@gmail.com>
2018-08-13 17:16:04 -0500

    Create ExcelSheetNumber.java

---
+++ string/ExcelSheetNumber.java
@@ -0,0 +1,10 @@
+class ExcelSheetNumber {
+    public int titleToNumber(String s) {
+        int res = 0;
+        for (int i = 0 ; i < s.length() ; i ++ ) {
+            res *= 26;
+            res += s.charAt(i) - 'A' + 1;
+        }
+        return res;
+    }
+}
