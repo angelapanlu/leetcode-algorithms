0b8dba2d851b45ab146e110a761448c2d3902a65
Xin Wang <angelapan.lu@gmail.com>
2018-08-20 21:08:04 -0500

    Create LCAOfBT.java

---
+++ tree/LCAOfBT.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ //Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+class LCAOfBT {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        return right;
+    }
+}
